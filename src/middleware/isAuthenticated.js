ac421031e895105c3b89b34501dadc326390a3ec
unknown <shresthamohanish123@gmail.com>
2025-05-02 17:51:27 +0545

    Update

--- src/middleware/isAuthenticated.js
+++ src/middleware/isAuthenticated.js
@@ -3,20 +3,7 @@ import jwt from "jsonwebtoken";
 import { secretkey1 } from "../constant.js";
 
 const isAuthenticated = expressAsyncHandler(async (req, res, next) => {
-  /* 
-    pass token from postman
-    get token
-    verify token
-    ifnot verify
-        throw error
-    if verify
-        extract id
-        pass id to exit middleware
-        next()
-    */
-
   let bearerToken = req.headers.authorization;
-  
 
   if (bearerToken) {
     let token = bearerToken.split(" ")[1];
